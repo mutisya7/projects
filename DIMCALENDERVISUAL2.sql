5866fc3920ea985d4e36fe4bdf7daf3961d5df70
Mutisya Eugine <eugenemutisyar27@gmail.com>
2023-01-12 12:30:39 +0300

    sales

---
+++ DIMCALENDERVISUAL2.sql
@@ -0,0 +1,14 @@
+-- collect necessary columns
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date, 
+  [EnglishDayNameOfWeek] AS Day, 
+  [EnglishMonthName] AS Month, 
+  Left([EnglishMonthName], 3) AS MonthShort,   -- Useful for front end date navigation and front end graphs.
+  [MonthNumberOfYear] AS MonthNo, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year
+FROM 
+ [AdventureWorksDW2019].[dbo].[DimDate]
+WHERE 
+  CalendarYear >= 2019
